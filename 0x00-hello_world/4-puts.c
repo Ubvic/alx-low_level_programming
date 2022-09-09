856a419e1093eeb471456c65b48b51bd6303f3d1
Uba_Victor <uvezionyinye@gmail.com>
2022-09-09 15:34:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,14 +6,12 @@
  * REturn: Always 0 (Successful)
  */
 
-        int main(void)
+ int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-        
 	/** 
 	 * return 0 because thats what you're supposed to do
 	 */
-
 	return (0);
 }
