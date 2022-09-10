598d679b8b4bcad56dfa68b986a431c4fa1cae93
Uba_Victor <uvezionyinye@gmail.com>
2022-09-09 17:45:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - prints a text
  *
- * Return: Always 1 (success)
+ * Return: Always 1.
  */
 
 int main(void)
 {
-	printf(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
