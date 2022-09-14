1e46186f2ff2c3fa9818e882eff5e1b2634cf287
Uba_Victor <uvezionyinye@gmail.com>
2022-09-14 16:24:28 -0700

    _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - gives the positive values of numbers
+ * Return: positive numbers
+ */
+
+int _abs(int)
+{
+	int i;
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+}
