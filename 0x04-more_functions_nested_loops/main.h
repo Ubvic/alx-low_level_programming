8d6cdee360aa7f8a993b707252ca76e746852514
Uba_Victor <uvezionyinye@gmail.com>
2022-09-15 16:40:30 -0700

    putchar

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,15 @@
+#include <unistd.h>
+
+/**
+ *_putchar - writes the character c to stdout
+ *@c: The character to print
+ *                                                    *On error, -1 is returned, and errno is set appropriately
+ */
+
+ int _putchar(char c)
+ {
+     return(write (1, &c, 1));
+ }
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
